eeca5b5134aa5195483d557ade37e1bd065d056f
Valentín <valentindeleo2001@gmail.com>
2025-10-19 21:49:06 -0300

    Celdas y columnas creadas

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -9,13 +9,27 @@ import entorno.InterfaceJuego;
 public class Juego extends InterfaceJuego {
 	private Entorno entorno = new Entorno(this, "Proyecto para TP", 1024, 768);
 	private InterfazPrueba interfaz;
-	private Celda celda;
+	//private Celda celda;
 	private Celda[] celdas;
 	
 	Juego() {
 		this.interfaz = new InterfazPrueba(512,84,168,1024,Color.gray);
 		this.celda = new Celda(84,228,120,120,Color.green);
-		this.celdas = new Celda[5];
+		int filas = 5;
+		int columnas = 10;
+		int posX = 75;
+		int posY = 250;
+		this.celdas = new Celda[filas * columnas];
+		int cont = 0;
+		for (int fil = 0; fil < filas; fil++) {
+			for (int colum = 0; colum < columnas; colum++) {
+				int x = (colum * 100) + posX;
+				int y = (fil * 100) + posY;
+				this.celdas[cont] = new Celda(x, y, 98, 98, Color.green);
+				cont++;
+				}
+				
+		}
 		this.entorno.iniciar();
 	}
 
@@ -23,8 +37,7 @@ public class Juego extends InterfaceJuego {
 	public void tick(){
 		this.interfaz.dibujar(entorno);
 		for (int i=0;i<celdas.length;i++) {
-			this.celda.dibujar(entorno);
-			this.celda.int x
+			this.celdas[i].dibujar(entorno);
 		}
 		
 	}
