d16012dbc1c31401e66ce102271082bf3dd62b4c
unknown <leomonzon9206@gmail.com>
2025-10-22 09:10:29 -0300

    mejora de tablero y entrada de zombies

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -12,7 +12,11 @@ public class Juego extends InterfaceJuego {
 	private InterfazPrueba interfaz;
 	//private Celda celda;
 	private Celda[] celdas;
+	private ZombieGrinch[] zombies;
 	Random random = new Random();
+	private int contadorTicks = 0;
+	private int intervaloSpawn =180; // empieza cada -3 seg.
+	private int ronda =1;
 	
 	Juego() {
 		this.interfaz = new InterfazPrueba(512,84,168,1024,Color.gray);
@@ -28,31 +32,59 @@ public class Juego extends InterfaceJuego {
 			for (int colum = 0; colum < columnas; colum++) {
 				int x = (colum * 100) + posX;
 				int y = (fil * 100) + posY;
-				this.celdas[cont] = new Celda(x, y, 98, 98, Color.green);
+				Color colorCelda;
+				if((fil+colum) %2 ==0){
+					colorCelda= new Color(102,204,102);
+				}
+				else {colorCelda = new Color (0,153,0);
+				}
+				this.celdas[cont]= new Celda(x,y,98,98, colorCelda);
 				cont++;
 				}
+		}
 				
-		}
+		this.zombies = new ZombieGrinch[15];
 		this.entorno.iniciar();
-	}
-	
-	public void iniciar() {
-		int x = 60;
-		for (int i = 0; i < celdas.length; i++) {
-			this.celdas[i] = new Celda(x,60,120,120, Color.green);
-			x++;
-		}
 	}
 
 
 	public void tick(){
 		this.interfaz.dibujar(entorno);
-
+		// dibujar celdas 
 		for (int i=0;i<celdas.length;i++) {
 			this.celdas[i].dibujar(entorno);
 		}
-
-
+		
+		for (int i = 0; i < zombies.length; i++) {
+			if (zombies[i] != null) {
+				zombies[i].dibujar(entorno);
+				zombies[i].caminar();
+			}	
+		}
+		//control de Spawn
+		contadorTicks++;
+		if (contadorTicks >= intervaloSpawn) {
+			agregarZombie();
+			contadorTicks=0;
+			// aumentar dificultad progresivamente 
+			if(intervaloSpawn >60);
+			intervaloSpawn-=10; //se acelera hasta 1 seg aprox.
+			if (ronda <10);
+			ronda++;
+		agregarZombie();
+		}
+	}
+	
+	private void agregarZombie() {
+        int[] posFila = {250, 350, 450, 550, 650}; 
+        int filaRandom = random.nextInt(5);
+        
+        for (int i = 0; i < zombies.length; i++) {
+            if (zombies[i] == null) {
+                zombies[i] = new ZombieGrinch(entorno.ancho(), posFila[filaRandom]);
+                return; 
+            }
+        }
     }
 
 	@SuppressWarnings("unused")
